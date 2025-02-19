9b5796c4402a115e533d0f27fbd287a9bf731e10
bhupinder2121221 <56498519+bhupinder2121221@users.noreply.github.com>
2025-02-19 17:03:36 +0530

    refactor build_files.sh file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
 # build_files.sh
 pip install -r requirements.txt
+
+echo "BUILD END"
